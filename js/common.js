3a46bf5cb80337d7425c69f193500c562f1917ca
John Kellyn Resman <resman.jk@gmail.com>
2017-11-08 16:24:24 -0600

    Release 1.3.0 - Color & Tiles
    Added project tiles, changed site color toggle, updated bio.

--- js/common.js
+++ js/common.js
@@ -1,18 +1,7 @@
 $(function(){
 
-  $('.switch input').click(function(){
-
-    if ($('body').hasClass('theme-day') ) {
-      $('body').removeClass();
-      $('body').addClass('theme-night');
-      $('.toggle-siteColor span').html('Night mode');
-    } else {
-      $('body').removeClass();
-      $('body').addClass('theme-day');
-      $('.toggle-siteColor span').html('Day mode');
-    }
-
-
-  });
+  $('#cb1').on('click', function () {
+     $('body').toggleClass('theme-day');
+   });
 
 });
