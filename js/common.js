5f579993d49750a1ab2fe142b4abb28d5a8f1c73
John Kellyn Resman <resman.jk@gmail.com>
2018-03-08 21:23:41 -0600

    v.2.0 - Simplified theme
    v2 ui

--- js/common.js
+++ js/common.js
@@ -1,29 +1,29 @@
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-function checkCookie() {
-    var theme = getCookie("theme");
-    if (theme == "theme-day") {
-        $('body').addClass('theme-day');
-        document.getElementById("cb1").checked = true;
-    } else {
-        $('body').addClass('theme-night');
-        document.getElementById("cb1").checked = false;
-    }
-}
-checkCookie()
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
+// function checkCookie() {
+//     var theme = getCookie("theme");
+//     if (theme == "theme-day") {
+//         $('body').addClass('theme-day');
+//         document.getElementById("cb1").checked = true;
+//     } else {
+//         $('body').addClass('theme-night');
+//         document.getElementById("cb1").checked = false;
+//     }
+// }
+// checkCookie()
 
 // DOC READY
 $(function(){
